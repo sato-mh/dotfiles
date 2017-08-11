d0576d1250fb650cd8f9f84cd376b3b3c10d2587
sato-mh <satomh37@gmail.com>
2017-08-11 23:19:54 +0900

    added

---
+++ set_symbolic_link.sh
@@ -0,0 +1,10 @@
+WORK_DIR=$(echo $(cd $(dirname $0) && pwd))
+ln -sf $WORK_DIR/.bashrc ~/.bashrc
+cp -r $WORK_DIR/.emacs.d ~/.emacs.d
+ln -sf $WORK_DIR/.emacs.d/init.el ~/.emacs.d/init.el
+ln -sf $WORK_DIR/.eslintrc.js ~/.eslintrc.js
+ln -sf $WORK_DIR/.flake8 ~/.flake8
+ln -sf $WORK_DIR/.gitignore ~/.gitignore
+ln -sf $WORK_DIR/.tmux.conf ~/.tmux.conf
+
+exec $SHELL
