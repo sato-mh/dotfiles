3e422ddbc7ced2fcf7f702499063af3b6bb9f95b
sato-mh <satomh37@gmail.com>
2017-09-29 18:29:13 +0900

    rename set-symbolic-links

--- set-symbolic-links.sh
+++ set-symbolic-links.sh
@@ -5,6 +5,7 @@ ln -sf $WORK_DIR/.emacs.d/init.el ~/.emacs.d/init.el
 ln -sf $WORK_DIR/.eslintrc.js ~/.eslintrc.js
 ln -sf $WORK_DIR/.flake8 ~/.flake8
 ln -sf $WORK_DIR/.gitignore ~/.gitignore
+ln -sf $WORK_DIR/.tern-config ~/.tern-config
 ln -sf $WORK_DIR/.tmux.conf ~/.tmux.conf
 
 exec $SHELL
