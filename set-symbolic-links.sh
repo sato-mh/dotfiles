cbb14f8b813eb9d6f386cd0e43990acb4ea64fa9
sato-mh <medias37@gmail.com>
2017-12-25 19:09:08 +0900

    Delete .gitconfig

--- set-symbolic-links.sh
+++ set-symbolic-links.sh
@@ -4,7 +4,6 @@ cp -r $WORK_DIR/.emacs.d ~/.emacs.d
 ln -sf $WORK_DIR/.emacs.d/init.el ~/.emacs.d/init.el
 ln -sf $WORK_DIR/.eslintrc.js ~/.eslintrc.js
 ln -sf $WORK_DIR/.flake8 ~/.flake8
-ln -sf $WORK_DIR/.gitconfig ~/.gitconfig
 ln -sf $WORK_DIR/.gitignore ~/.gitignore
 ln -sf $WORK_DIR/.inputrc ~/.inputrc
 ln -sf $WORK_DIR/.tern-config ~/.tern-config
