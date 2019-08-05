74c5d6fddfc0a69259d8d85dc40a3821cbf43c48
satomh <satomh@gmail.com>
2019-08-05 18:50:05 +0900

    Fix bugs

--- bin/set-symbolic-links.sh
+++ bin/set-symbolic-links.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-WORK_DIR=$(echo $(cd $(dirname $0) && pwd))
-ln -sf $WORK_DIR/../linux/.bashrc ~/.bashrc
-mkdir -p ~/.emacs.d
-ln -sf $WORK_DIR/../linux/.emacs.d/init.el ~/.emacs.d/init.el
-ln -sf $WORK_DIR/../linux/.eslintrc.js ~/.eslintrc.js
-ln -sf $WORK_DIR/../linux/.flake8 ~/.flake8
-ln -sf $WORK_DIR/../linux/.gitignore ~/.gitignore
-ln -sf $WORK_DIR/../linux/.inputrc ~/.inputrc
-ln -sf $WORK_DIR/../linux/.tern-config ~/.tern-config
-ln -sf $WORK_DIR/../linux/.tmux.conf ~/.tmux.conf
+WORK_DIR=$(echo $(cd $(dirname $0)/../ && pwd))
+ln -sf $WORK_DIR/linux/.bashrc ~/.bashrc
+mkdir -p ~/.emacs.d/site-lisp
+ln -sf $WORK_DIR/.emacs.d/init.el ~/.emacs.d/init.el
+ln -sf $WORK_DIR/linux/.eslintrc.js ~/.eslintrc.js
+ln -sf $WORK_DIR/linux/.flake8 ~/.flake8
+ln -sf $WORK_DIR/linux/.gitignore ~/.gitignore
+ln -sf $WORK_DIR/linux/.inputrc ~/.inputrc
+ln -sf $WORK_DIR/linux/.tern-config ~/.tern-config
+ln -sf $WORK_DIR/linux/.tmux.conf ~/.tmux.conf
 
 exec $SHELL
