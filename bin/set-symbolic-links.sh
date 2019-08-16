6834b7f6c7e9572c85d3ef4f3c40d8c62b40b7ef
satomh <satomh@gmail.com>
2019-08-16 18:35:50 +0900

    Add yapf config

--- bin/set-symbolic-links.sh
+++ bin/set-symbolic-links.sh
@@ -6,6 +6,8 @@ mkdir -p ~/.emacs.d/site-lisp
 ln -sf $WORK_DIR/.emacs.d/init.el ~/.emacs.d/init.el
 ln -sf $WORK_DIR/linux/.eslintrc.js ~/.eslintrc.js
 ln -sf $WORK_DIR/linux/.flake8 ~/.flake8
+mkdir -p ~/.config/yapf
+ln -sf $WORK_DIR/linux/config/yapf/.style.yapf ~/.config/yapf/style
 ln -sf $WORK_DIR/linux/.gitignore ~/.gitignore
 ln -sf $WORK_DIR/linux/.inputrc ~/.inputrc
 ln -sf $WORK_DIR/linux/.tern-config ~/.tern-config
